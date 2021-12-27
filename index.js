fdf35cb3f8dfa37c1e144625310a182bae64e508
ericklima-ca <ericklima.ca@yahoo.com>
2021-12-27 16:40:59 +0000

    fix config for production

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const { sequelize } = require("./models");
 const app = require("./server/app/app");
+const http = require("http").Server(app);
 const socketIO = require("socket.io");
 
-const io = socketIO(app);
+const io = socketIO(http);
 const port = process.env.PORT || 3000;
 
 //require("dotenv").config();
 
-app.listen(port, async () => {
+http.listen(port, async () => {
   console.log("listening on port " + port);
 });
 
