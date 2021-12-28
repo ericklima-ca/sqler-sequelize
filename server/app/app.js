3927e293a1383f975a4fa8fb5b15e1f9d901a8e6
ericklima-ca <ericklima.ca@yahoo.com>
2021-12-27 22:21:20 -0400

    fixing cors config

--- server/app/app.js
+++ server/app/app.js
@@ -5,14 +5,7 @@ const authController = require("./controllers/auth");
 const solicitationController = require("./controllers/solicitation");
 const responseController = require("./controllers/response");
 
-const customCors = {
-  origin: ["*", "http://localhost:3200"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
-
-app.use(cors(customCors));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/auth", authController);
