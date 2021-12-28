b2588f1ec18e4e0c6797c33468eab3b7983f9012
ericklima-ca <ericklima.ca@yahoo.com>
2021-12-27 22:33:03 -0400

    enable cors in socket io

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const app = require("./server/app/app");
 const http = require("http").Server(app);
 const socketIO = require("socket.io");
 
-const io = socketIO(http);
+const enabledCors = {
+  cors: {
+    origin: "*",
+    methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  },
+};
+
+const io = socketIO(http, enabledCors);
 const port = process.env.PORT || 3000;
 
 //require("dotenv").config();
